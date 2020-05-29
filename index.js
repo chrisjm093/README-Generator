c599fe9270f330b20d8688f573b83e64813b17d4
Christopher Marti <chrisjm093@gmail.com>
2020-05-28 17:10:14 -0700

    Question list made and formatted for Node, Table of contents module constructed.

--- index.js
+++ index.js
@@ -1,25 +1,78 @@
 const inquirer = require('inquirer');
-
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown.js")
 const questions = [ 
     {
     type: "input",
     message: "Enter a project Title",
-    name: "projectTitle"
+    name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter project description.",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Enter installation instructions.",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "Enter usage information.",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Enter Licence.",
+        name: "licence"
+    },
+    {
+        type: "input",
+        message: "Who were the contributers?",
+        name: "contributers"
+    },
+    {
+        type: "input",
+        message: "Describe the testing involved in this project.",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "Past a link to your github profile picture",
+        name: "picLink"
+    },
+    {
+        type: "input",
+        message: "What is the Email associated with your Github?",
+        name: "email"
     }
 ];
 
 function writeToFile(fileName, data) {
+
+    fs.writeFile( fileName, generateMarkdown(data), function(error) {
+        if( error ) console.log( error );
+        else console.log( 'Success!' );
+    });
+
+
 }
+
+
+
 
 function init() {
 
+    inquirer
+        .prompt( questions )
+        .then( ( response ) => {
 
-
-    inquirer.prompt( questions ).then( ( response ) => {
-
-        console.log( response );
-
-    })
+            writeToFile( 'README.md', response );
+        
+        
+       
+    } );
 
 }
 
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,40 @@
+function generateMarkdown(data) {
+  console.log(data)
+  return `
+  ${ generateTitle( data.title )}
+
+  ${ generateDescription( data.description )}
+
+  ${ generateToC( data.ToC ) }
+  `; 
+}
+
+function generateTitle( title ){
+  console.log( title )
+  return `
+   # ${title}
+`
+}
+
+function generateDescription( description ){
+  return `
+  <a name="desc"></a>
+  ## ${description}`
+}
+
+function generateToC( ToC ){
+  if( ToC === true ){
+    return`
+    #Table of Contents
+   1. [ Description ] (#desc)
+   2. [ Installation ] (#install)
+   3. [Usage] (#usage)
+   4. [Licence] (#licence)
+   5. [Contributing] (#contributing)
+   6. [Tests] (#tests)
+   7. [GitHub Profile Picture] (#profilePic)
+   8. [GitHub Email] (#email)
+  `
+  }
+}
+module.exports = generateMarkdown;
